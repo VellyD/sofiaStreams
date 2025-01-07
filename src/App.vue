981c903f2f6795910f8923a9a070e1f1fe132794
VellyD <viddvd3@gmail.com>
2025-01-07 15:16:47 +0200

    Adding starter pack and setting up project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import AppFooter from "./components/AppFooter.vue";
+  //
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+    <AppFooter />
+  </v-app>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<script>
+  //
+</script>
+
+<template>
+  <v-btn
+    rounded="xl"
+    bg-color="red"
+    size="x-large"
+    block
+    variant="outlined"
+  >
+    Save
+  </v-btn>
+</template>
+
+<style></style>
