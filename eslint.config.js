72e04a48802a3e73fa4abc0d4d54d904f9559791
VellyD <viddvd3@gmail.com>
2025-01-11 18:29:08 +0200

    es lint/prettier update

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,87 @@
-import js from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from 'eslint-define-config';
 
-export default [
-  {
-    name: "app/files-to-lint",
-    files: ["**/*.{js,mjs,jsx,vue}"],
+export default defineConfig({
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-
-  {
-    name: "app/files-to-ignore",
-    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
-
-  // js.configs.recommended,
-  // ...pluginVue.configs["flat/recommended"],
-
-  // {},
-  {
-    extends: [
-      "eslint:recommended",
-      "plugin:vue/vue3-recommended",
-      "plugin:prettier/recommended",
+  rules: {
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style'],
+      },
+    ],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'ignore',
+        multiline: 'below',
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 2,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+      },
+    ],
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/script-setup-uses-vars': 'error',
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'props',
+          'emits',
+          'data',
+          'watch',
+          'computed',
+          'mounted',
+          'methods',
+        ],
+      },
     ],
   },
-  {
-    rules: {
-      "vue/component-tags-order": [
-        "error",
-        {
-          order: ["script", "template", "style"], // Enforce this order
-        },
-      ],
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
     },
-  },
-];
+  ],
+});
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <script>
-  import Header from './components/Header.vue';
-  import Footer from './components/Footer.vue';
-  import Contacts from './pages/Contacts.vue';
-  import Login from './pages/Login.vue';
-  import Register from './pages/Register.vue';
-  import Home from './pages/Home/Home.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Contacts from './pages/Contacts.vue';
+import Login from './pages/Login.vue';
+import Register from './pages/Register.vue';
+import Home from './pages/Home/Home.vue';
 
-  export default {
-    components: { Footer, Header, Contacts, Login, Register, Home },
-    data() {
-      return {
-        view: 'Home',
-      };
+export default {
+  components: {
+    Footer,
+    Header,
+    Contacts,
+    Login,
+    Register,
+    Home,
+  },
+  data() {
+    return {
+      view: 'Home',
+    };
+  },
+  methods: {
+    onNavigationSelect(selectedView) {
+      this.view = selectedView;
     },
-    methods: {
-      onNavigationSelect(selectedView) {
-        this.view = selectedView;
-      },
-    },
-  };
+  },
+};
 </script>
 
 <template>
   <v-app>
-    <Header
-      :is-auth-user="false"
-      @on-select="onNavigationSelect"
-    />
+    <Header :is-auth-user="false" @on-select="onNavigationSelect" />
     <v-main>
       <component :is="view" />
       <!-- <router-view /> -->
