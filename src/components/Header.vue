9dae88497bb1c3c815c4aa8eb607832ca065fdf9
VellyD <viddvd3@gmail.com>
2025-01-14 18:46:35 +0200

    Setting up the router and adding the routes.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,68 +1,84 @@
 <script>
-export default {
-  props: {
-    isAuthUser: {
-      type: Boolean,
-      required: true,
+  export default {
+    props: {
+      isAuthUser: {
+        type: Boolean,
+        required: true,
+      },
     },
-  },
-  emits: ['onSelect'],
-  methods: {
-    onSelect(view) {
-      this.$emit('onSelect', view);
-    },
-  },
-};
+  };
 </script>
 
 <template>
-  <v-app-bar :elevation="2" color="background">
+  <v-app-bar
+    :elevation="2"
+    color="background"
+  >
     <v-app-bar-title>
-      <v-icon size="28" color="primary" class="mb-2"> mdi-fountain </v-icon>
-      The Fountains of Sofia
+      <v-icon
+        size="28"
+        color="primary"
+        class="mb-2"
+      >
+        mdi-chef-hat
+      </v-icon>
+      Yum Yum recipes
     </v-app-bar-title>
 
-    <v-tabs v-if="!isAuthUser" color="text" align="center">
-      <v-tab value="Home" @click.prevent="onSelect('Home')">
-        <v-icon class="me-2">mdi-home</v-icon>
-        Home
-      </v-tab>
-
-      <v-tab value="Login" @click.prevent="onSelect('Login')">
-        <v-icon class="me-2">mdi-login-variant</v-icon>
-        Login
-      </v-tab>
-
-      <v-tab value="Register" @click.prevent="onSelect('Register')">
-        <v-icon class="me-2">mdi-account-plus</v-icon>
-        Register
-      </v-tab>
-
-      <v-tab value="Favorites" @click.prevent="onSelect('Contacts')">
-        <v-icon class="me-2">mdi-contacts</v-icon>
-        Contacts
-      </v-tab>
-    </v-tabs>
-    <v-tabs v-else color="primary" align="center">
-      <v-tab value="Home" @click.prevent="onSelect('Home')">
+    <v-tabs
+      v-if="!isAuthUser"
+      color="text"
+      align="center"
+    >
+      <v-tab
+        value="Home"
+        to="/"
+      >
         <v-icon class="me-2">mdi-home</v-icon>
         Home
       </v-tab>
 
       <v-tab
-        value="Add Fountain/ Location"
-        @click.prevent="onSelect('Add Fountain/ Location')"
+        value="Login"
+        to="/login"
       >
-        <v-icon class="me-2">mdi-view-grid-plus</v-icon>
-        Add Fountain/ Location
+        <v-icon class="me-2">mdi-login-variant</v-icon>
+        Login
       </v-tab>
 
-      <v-tab value="Favorites" @click.prevent="onSelect('Favorites')">
+      <v-tab
+        value="Register"
+        to="/register"
+      >
+        <v-icon class="me-2">mdi-account-plus</v-icon>
+        Register
+      </v-tab>
+    </v-tabs>
+    <v-tabs
+      v-else
+      color="primary"
+      align="center"
+    >
+      <v-tab
+        value="Home"
+        to="/"
+      >
+        <v-icon class="me-2">mdi-home</v-icon>
+        Home
+      </v-tab>
+
+      <v-tab
+        value="Favorites"
+        to="/favorites"
+      >
         <v-icon class="me-2">mdi-heart</v-icon>
         Favorites
       </v-tab>
 
-      <v-tab value="Profile" @click.prevent="onSelect('Profile')">
+      <v-tab
+        value="Profile"
+        to="/profile"
+      >
         <v-icon class="me-2">mdi-account-cog</v-icon>
         Profile
       </v-tab>
