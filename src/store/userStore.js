20f9cecf5a0d2fcc4bde0bf3a490ec6b5479a0d5
VellyD <viddvd3@gmail.com>
2025-01-16 20:09:43 +0200

    Adding authentication and updating the routing 404

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -5,12 +5,30 @@ export const useUserStore = defineStore("user", {
     return {
       isAuthenticated: false,
       profile: null,
+      favoritesIDs: [],
     };
   },
   actions: {
     setProfile(userData) {
       this.profile = userData;
+      sessionStorage.setItem("user", JSON.stringify(userData));
       this.isAuthenticated = true;
+    },
+    addFavorite(id) {
+      this.favoritesIDs.push(id);
+    },
+    removeFavorite(id) {
+      this.favoritesIDs = this.favoritesIDs.filter((fav) => fav !== id);
+    },
+    getUserToken() {
+      if (!sessionStorage.getItem("user")) return;
+      this.profile = JSON.parse(sessionStorage.getItem("user"));
+      this.isAuthenticated = true;
+    },
+    logout() {
+      this.isAuthenticated = false;
+      this.profile = null;
+      sessionStorage.removeItem("user");
     },
   },
 });
