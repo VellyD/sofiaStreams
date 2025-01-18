b2e43a104736e77cef485df73fc32cf53b6caf4a
VellyD <viddvd3@gmail.com>
2025-01-18 15:11:52 +0200

    Adding alert for when the credentials are wrong.

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,9 +3,8 @@ import axios from "axios";
 export async function logInUser(user) {
   try {
     const res = await axios.post("https://dummyjson.com/auth/login", user);
-    return res.data;
+    return res;
   } catch (e) {
-    console.error(e);
-    return null;
+    return e;
   }
 }
